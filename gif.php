e342021eff391f5b9d034150dad0712363bae4b8
Steven Aston <steven@Steve.local>
2016-02-08 13:09:06 -0500

    adding second line

--- gif.php
+++ gif.php
@@ -47,5 +47,6 @@ ob_end_clean();
 //header( 'Cache-Control: no-store, no-cache, must-revalidate' );
 //header( 'Cache-Control: post-check=0, pre-check=0', false );
 //header( 'Pragma: no-cache' );
+header('Content-Type: image/png');
 $png = $image;
 $png->display();
